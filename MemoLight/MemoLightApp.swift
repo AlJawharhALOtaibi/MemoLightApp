1bb77009f4fd19b1ec56a5be70476417062e9085
AlJawharhALOtaibi <ALJawharhALOtaibi@gmail.com>
2024-04-18 14:09:47 +0300

    Initial Commit

---
+++ MemoLight/MemoLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoLightApp.swift
+//  MemoLight
+//
+//  Created by AlJawharh AlOtaibi on 09/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MemoLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
